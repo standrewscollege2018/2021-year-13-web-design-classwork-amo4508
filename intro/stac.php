beae07d4e38966fc136702dc8aa983f1b0448823
amo4508 <60507791+amo4508@users.noreply.github.com>
2021-02-10 14:20:29 +1300

    intro

---
+++ intro/stac.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="custom.css">
+    <title></title>
+  </head>
+  <body>
+    <div class="container_fluid">
+      <div class="row">
+        <div class="navbar">
+            <?php
+
+              include "stacnavbar.html";
+              ?>
+
+        </div>
+
+      </div>
+    </div>
+  </body>
+</html>
